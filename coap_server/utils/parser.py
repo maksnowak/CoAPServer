7865e665c163bd7cfcee399d890736c0c1124f0c
maksnowak <59841779+maksnowak@users.noreply.github.com>
2025-01-10 20:01:43 +0100

    Add message parsing

--- coap_server/utils/parser.py
+++ coap_server/utils/parser.py
@@ -11,4 +11,16 @@ def parse_request(data: bytes) -> CoapRequest:
     :return: CoAP request data as a dictionary
     :rtype: CoapRequest
     """
-    raise NotImplementedError("parse_request function not implemented.")
+    header_version = (0xC0 & data[0]) >> 6
+    header_type    = (0x30 & data[0]) >> 4
+    header_tkl     = (0x0F & data[0]) >> 0
+    header_class   = (data[1] >> 5) & 0x07
+    header_code    = (data[1] >> 0) & 0x1F
+    header_mid     = (data[2] << 8) | data[3]
+    token          = data[4:4 + header_tkl]
+    options        = data[12:16]
+    if data[16] == 0xFF: # payload marker
+        payload = data[17:]
+    else:
+        payload = None
+    
