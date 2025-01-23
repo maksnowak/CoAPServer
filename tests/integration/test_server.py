70614b01501b44d345761e28da5ad1fab04bae12
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-01-23 21:48:58 +0100

    added server tests and shutdown for server

---
+++ tests/integration/test_server.py
@@ -0,0 +1,50 @@
+import socket
+from threading import Thread
+
+from coap_server.server import CoAPServer
+from coap_server.utils.constants import CoapMessage, CoapOption, CoapCode
+from coap_server.utils.parser import encode_message, parse_message
+
+
+def client():
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+        server_address = ("127.0.0.1", 5683)
+
+        request = CoapMessage(
+            header_version=1,
+            header_type=0,
+            header_token_length=4,
+            header_code=CoapCode.GET,
+            header_mid=1337,
+            token=b"1234",
+            options={
+                CoapOption.URI_PATH: b"/temperature/1",
+            },
+            payload=b"",
+        )
+
+        sock.sendto(encode_message(request), server_address)
+
+        data = sock.recv(1024)
+        response = parse_message(data)
+        return response
+
+
+def test_get():
+    server = CoAPServer()
+    server_thread = Thread(target=server.start, daemon=True)
+    server_thread.start()
+
+    response = client()
+
+    assert response.header_version == 1
+    assert response.header_type == 0
+    assert response.header_token_length == 4
+    assert response.header_code == CoapCode.CONTENT
+    assert response.header_mid == 1337
+    assert response.token == b"1234"
+    assert response.options == {}
+    assert response.payload == b"Temperature is 22C"
+
+    server.shutdown()
+    server_thread.join()
