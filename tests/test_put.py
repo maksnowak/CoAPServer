118602066aeb81676e62626796e19de0a94b79d2
Michał Machnikowski <01178559@pw.edu.pl>
2025-01-23 13:43:47 +0100

    Improved PUT test with checking if resource was actually modified

--- tests/test_put.py
+++ tests/test_put.py
@@ -31,3 +31,30 @@ def test_success():
     assert response.token == b"1234"
     assert response.options == {}
     assert response.payload == b'{"name": "New name"}'
+
+    # Check it was actually modified
+    request = CoapMessage(
+        header_version=1,
+        header_type=0,
+        header_token_length=4,
+        header_code=CoapCode.GET,
+        header_mid=1337,
+        token=b"1234",
+        options={
+            CoapOption.URI_PATH: b"/devices/1",
+        },
+        payload=b"",
+    )
+
+    request_encoded = encode_message(request)
+    response_encoded = handler.handle_request(request_encoded)
+    response = parse_message(response_encoded)
+
+    assert response.header_version == 1
+    assert response.header_type == 0
+    assert response.header_token_length == 4
+    assert response.header_code == CoapCode.CONTENT
+    assert response.header_mid == 1337
+    assert response.token == b"1234"
+    assert response.options == {}
+    assert response.payload == b'{"name": "New name"}'
