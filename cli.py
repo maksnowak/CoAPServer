b358c9b53d5cb5c6f082a928a1820c401ee994ca
Michał Machnikowski <01178559@pw.edu.pl>
2025-01-24 06:40:19 +0100

    Specified default port in `cli.py`

--- cli.py
+++ cli.py
@@ -1,15 +1,19 @@
 import socket
 from urllib.parse import urlparse
+
 import typer
+
 from coap_server.utils.constants import CoapCode, CoapMessage, CoapOption
-from coap_server.utils.parser import parse_message, encode_message
+from coap_server.utils.parser import encode_message, parse_message
 
 app = typer.Typer()
 
 
 @app.command()
 def request(
-    method: str = typer.Option("GET", help="HTTP method (GET/POST/PUT/DELETE)"),
+    method: str = typer.Option(
+        "GET", help="HTTP method (GET/POST/PUT/DELETE)"
+    ),
     uri: str = typer.Argument(..., help="CoAP URI (coap://host:port/path)"),
     data: str = typer.Option(None, help="Data to send in request body"),
 ):
@@ -21,7 +25,7 @@ def request(
         raise typer.BadParameter("URI must use coap:// scheme")
 
     host = parsed_uri.hostname
-    port = parsed_uri.port
+    port = parsed_uri.port or 5683
     path = parsed_uri.path
 
     # Map method string to CoapCode enum
